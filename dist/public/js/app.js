71f07746459ba3b222c79cd3ee4acc3dd939bd5f
marceltheshell <marcel.degas@gmail.com>
2018-04-18 01:33:32 -0700

    final commit for tonight

--- dist/public/js/app.js
+++ dist/public/js/app.js
@@ -1,28 +1,135 @@
-"use strict";
+'use strict';
 
-var rootUrl = 'https://www.westelm.com/services/catalog/v4/category/shop/new/all-new/index.json';
-
-var products = [];
-var numberOfProducts = 0;
-var moreData = [];
-
-var fetchData = function fetchData(url, callback) {
-  fetch(url, { method: 'GET' }).then(function (data) {
-    return data.json();
-  }).then(function (json) {
-    callback(json);
-  }).catch(function (error) {
-    return handleError(error);
-  });
-};
-
-var handleError = function handleError(e) {
-  throw new Error('There was an error fetching this data with error: ' + e);
-};
-
-fetchData(rootUrl, function (json) {
-  products = json.groups;
-  // console.log("here")
-  // console.log("products" + products)
-  numberOfProducts = json.groups.length;
+fetch('/wsiData', { method: 'GET' }).then(function (data) {
+  return data.json();
+}).then(function (json) {
+  appendData(json);
+}).catch(function (error) {
+  return console.log(error);
 });
+
+var getPrice = function getPrice(jsonPrice) {
+  var priceRange = jsonPrice["priceRange"];
+  var price = jsonPrice["price"];
+  var productPrice = null;
+
+  if (priceRange === undefined) {
+    productPrice = '$ ' + price["selling"];
+  } else if (price === undefined) {
+    var low = priceRange["selling"]["low"];
+    var high = priceRange["selling"]["high"];
+    productPrice = '$ ' + low + ' - $ ' + high;
+  }
+  return productPrice;
+};
+
+var serialize = function serialize(blob) {
+
+  var response = {};
+  var images = [];
+
+  for (var i = 0; i < blob["images"].length; i++) {
+    images.push(blob["images"][i]["href"]);
+  }
+  // if no images, add the hero img
+  if (images.length <= 0) {
+    images.push(blob["hero"]["href"]);
+  }
+
+  response["images"] = images;
+  response["price"] = getPrice(blob);
+  response["name"] = blob["name"].toUpperCase();
+  response["hero"] = blob["hero"]["href"];
+
+  return response;
+};
+
+var appendCarouselIndicators = function appendCarouselIndicators(thumbnails) {
+  var carouselIndicators = document.getElementById("carousel-indicators");
+  carouselIndicators.innerHTML = "";
+
+  for (var i = 0; i < thumbnails.length; i++) {
+
+    //creating the indicator li
+    var li = document.createElement("li");
+    li.dataset.dataSlideTo = i.toString();
+    li.dataset.target = "#carousel";
+
+    // setting first element to active class
+    if (i <= 0) {
+      li.className = "active";
+    }
+    carouselIndicators.appendChild(li);
+  }
+};
+
+var appendCarouselImgs = function appendCarouselImgs(thumbnails) {
+  var element = document.getElementById("carousel-inner");
+  element.innerHTML = "";
+
+  for (var i = 0; i < thumbnails.length; i++) {
+    var carouselItem = document.createElement("div");
+
+    if (i <= 0) {
+      carouselItem.className = "carousel-item active";
+    } else {
+      carouselItem.className = "carousel-item";
+    }
+
+    var img = document.createElement("img");
+    img.className = "d-block w-100";
+    img.src = thumbnails[i];
+    carouselItem.appendChild(img);
+    element.appendChild(carouselItem);
+  }
+};
+
+var showCarousel = function showCarousel(imgThumbnails) {
+  $('#myModal').modal('show');
+  appendCarouselImgs(imgThumbnails);
+  appendCarouselIndicators(imgThumbnails);
+};
+
+var appendData = function appendData(json) {
+  var _loop = function _loop(i) {
+    //serializing response
+    var response = serialize(json["groups"][i]);
+
+    // creating DOM variables
+    var liNode = document.createElement("li");
+    liNode.className = "list-inline-item col-xs-12 col-sm-4 li-item";
+
+    var divOuter = document.createElement("div");
+    divOuter.className = "outer";
+
+    var img = document.createElement("img");
+    img.className = "img";
+    img.src = response["hero"];
+    img.addEventListener("click", function () {
+      showCarousel(response["images"]);
+    }, false);
+
+    var cardName = document.createElement("div");
+    cardName.className = "card-text container";
+
+    var cardPrice = document.createElement("div");
+    cardPrice.className = "card-price";
+
+    var nameNode = document.createTextNode(response["name"]);
+    var priceNode = document.createTextNode(response["price"]);
+
+    //appending DOM variables
+    cardName.appendChild(nameNode);
+    cardPrice.appendChild(priceNode);
+    divOuter.appendChild(cardName);
+    divOuter.appendChild(cardPrice);
+    divOuter.appendChild(img);
+    liNode.appendChild(divOuter);
+
+    document.getElementById("products").appendChild(liNode);
+  };
+
+  for (var i = 0; i < json["groups"].length; i++) {
+    _loop(i);
+  }
+};
--- public/js/app.js
+++ public/js/app.js
@@ -1,27 +1,129 @@
-"use strict";
-
-const rootUrl = 'https://www.westelm.com/services/catalog/v4/category/shop/new/all-new/index.json'
-
-let products = []
-let numberOfProducts = 0
-let moreData = []
-
-const fetchData = (url, callback) => {
-  fetch(url, { method: 'GET' })
-    .then(data => data.json())
+fetch('/wsiData', { method: 'GET' })
+    .then(data =>  data.json())
     .then(json => {
-      callback(json);
+      appendData(json)
     })
-    .catch((error) => handleError(error));
-}
+    .catch((error) => console.log(error));
 
-const handleError = (e) => {
-  throw new Error(`There was an error fetching this data with error: ${e}`);
+let getPrice = (jsonPrice) => {
+  let priceRange = jsonPrice["priceRange"];
+  let price = jsonPrice["price"];
+  let productPrice = null;
+  
+  if (priceRange === undefined) {
+    productPrice = `$ ${price["selling"]}`;
+
+  } else if (price === undefined) {
+    const low = priceRange["selling"]["low"];
+    const high = priceRange["selling"]["high"];
+    productPrice = `$ ${low} - $ ${high}` 
+  }
+  return productPrice;
 };
 
-fetchData(rootUrl, json => {
-  products = json.groups
-  // console.log("here")
-  // console.log("products" + products)
-  numberOfProducts = json.groups.length
-})
+
+let serialize = (blob) => {
+  
+  let response = {};
+  let images = [];
+
+  for (let i = 0; i < blob["images"].length; i++) {
+    images.push(blob["images"][i]["href"]);
+  }
+  // if no images, add the hero img
+  if (images.length <= 0) {
+    images.push(blob["hero"]["href"]);
+  }
+
+  response["images"] = images;
+  response["price"] = getPrice(blob);
+  response["name"] = blob["name"].toUpperCase();
+  response["hero"] = blob["hero"]["href"];
+
+  return response;
+};
+
+let appendCarouselIndicators = (thumbnails) => {
+  const carouselIndicators = document.getElementById("carousel-indicators");
+  carouselIndicators.innerHTML = "";
+
+  for (let i = 0; i < thumbnails.length; i++) {
+    
+    //creating the indicator li
+    const li = document.createElement("li");
+    li.dataset.dataSlideTo = i.toString();
+    li.dataset.target = "#carousel";
+    
+    // setting first element to active class
+    if (i <= 0) {
+      li.className = "active";
+    } 
+    carouselIndicators.appendChild(li);
+  }
+};
+
+let appendCarouselImgs = (thumbnails) => {
+  const element = document.getElementById("carousel-inner");
+  element.innerHTML = "";
+
+  for (let i = 0; i < thumbnails.length; i++) {
+    const carouselItem = document.createElement("div");
+
+    if (i <= 0) {
+      carouselItem.className = "carousel-item active";
+    } else {
+      carouselItem.className = "carousel-item";
+    }
+
+    const img = document.createElement("img");
+    img.className = "d-block w-100";
+    img.src = thumbnails[i];
+    carouselItem.appendChild(img);
+    element.appendChild(carouselItem);
+  }
+};
+
+let showCarousel = (imgThumbnails) => {
+  $('#myModal').modal('show')
+  appendCarouselImgs(imgThumbnails);
+  appendCarouselIndicators(imgThumbnails)
+};
+
+let appendData = (json) => {
+
+  for (let i = 0; i < json["groups"].length; i++) {
+    //serializing response
+    const response = serialize(json["groups"][i])
+
+    // creating DOM variables
+    const liNode = document.createElement("li");
+    liNode.className = "list-inline-item col-xs-12 col-sm-4 li-item";
+
+    const divOuter = document.createElement("div");
+    divOuter.className = "outer";
+
+    const img = document.createElement("img");
+    img.className = "img";
+    img.src = response["hero"];
+    img.addEventListener("click", function() { showCarousel(response["images"]) }, false);
+
+    const cardName = document.createElement("div");
+    cardName.className = "card-text container";
+
+    const cardPrice = document.createElement("div");
+    cardPrice.className = "card-price";
+
+    const nameNode = document.createTextNode(response["name"]);
+    const priceNode = document.createTextNode(response["price"]);
+
+    //appending DOM variables
+    cardName.appendChild(nameNode);
+    cardPrice.appendChild(priceNode);
+    divOuter.appendChild(cardName)
+    divOuter.appendChild(cardPrice)
+    divOuter.appendChild(img);
+    liNode.appendChild(divOuter);
+
+    document.getElementById("products").appendChild(liNode);
+  }
+};
